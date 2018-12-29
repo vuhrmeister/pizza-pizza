d12c84a8781406d3b09e77d09738b9bd75204f9b
Valentin Uhrmeister <kontakt@ufisch.net>
2018-12-29 16:16:59 +0100

    Menus must have an id

--- modules/menu/fixtures.js
+++ modules/menu/fixtures.js
@@ -1,21 +1,26 @@
 module.exports = [
   {
+    id: 0,
     name: 'Pizza Margarita',
     price: 5
   },
   {
+    id: 1,
     name: 'Pizza Funghi',
     price: 6
   },
   {
+    id: 2,
     name: 'Pizza Gorgonzola',
     price: 6
   },
   {
+    id: 3,
     name: 'Pizza Calabrese',
     price: 7
   },
   {
+    id: 4,
     name: 'Pizza à la Chef',
     price: 8.5
   }
